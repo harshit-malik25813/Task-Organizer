2eae3816fdd9dfc9db6e423f47047b289e0bc965
harshit-malik25813 <harshitfr25@gmail.com>
2026-06-13 11:05:32 +0530

    Initals

--- main/connector.c
+++ main/connector.c
@@ -1,2 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 // Additional feature to manage project using improve-cli tool
+int main(void)
+{
+    printf("connector\n");
+    printf("This will help this tool to extend its functionality \nby integrating improve-cli, an open source project management tool to help you track your \nprogress in various project by entering tasks accomplished in the same.\n");
+    printf("This program will now execute some commands on your system to manage and install improve-cli\n");
+}
